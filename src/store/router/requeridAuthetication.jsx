4976b506c4dbc3947d420b349497643cf62bba12
Gabriel Carlos Cavalcante <80012329+Gabrielttbr@users.noreply.github.com>
2022-08-08 17:47:32 -0300

    bug do styled-component resolvido

--- src/store/router/requeridAuthetication.jsx
+++ src/store/router/requeridAuthetication.jsx
@@ -3,7 +3,9 @@ import { Navigate, Outlet } from "react-router-dom";
 import IsAutheticed from "../provider/isAuthetication";
 
 const RequiredAuthetication = () => {
-    console.log(IsAutheticed())
-    return <Outlet/>
+    if (IsAutheticed() === true){
+        return <Outlet/>
+    }
+    return <Navigate to="/login"/>
 }
 export default RequiredAuthetication;
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export const Global = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=New+Tegomin&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300&display=swap');
+
 *{
     margin: 0;
     padding: 0;
